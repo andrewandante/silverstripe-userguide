0b7c18d9268feedf4fd32b8ed4733afa41c167ad
Andrew Paxley <andrew.paxley@silverstripe.com>
2021-10-12 23:35:31 +1300

    add DerivedClass, summary fields and ReadOnly UserGuide fields

--- src/Model/UserGuide.php
+++ src/Model/UserGuide.php
@@ -2,15 +2,37 @@
 
 namespace SilverStripe\UserGuide\Model;
 
+use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataObject;
 
 class UserGuide extends DataObject
 {
+    private static $table_name = 'UserGuide';
+
     private static $db = [
         'Title' => 'Varchar',
         'PreNotes' => 'HTMLText',
         'Content' => 'HTMLText',
         'PostNotes' => 'HTMLText',
         'MarkdownPath' => 'Varchar',
+        'DerivedClass' => 'Varchar',
     ];
+
+    private static $summary_fields = [
+        'Title' => 'File Name',
+        'DerivedClass' => 'Class',
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->makeFieldReadonly([
+            'Title',
+            'Content',
+            'MarkdownPath',
+            'DerivedClass',
+        ]);
+
+        return $fields;
+    }
 }
