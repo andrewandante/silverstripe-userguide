6030f8fce7e9499765a31ba574a33304b96e8c42
Andrew Paxley <andrew.paxley@silverstripe.com>
2021-10-12 15:41:34 +1300

    remove add new button model admin

--- src/Admin/UserGuideAdmin.php
+++ src/Admin/UserGuideAdmin.php
@@ -3,6 +3,8 @@
 namespace SilverStripe\UserGuide\Admin;
 
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldConfig;
 use SilverStripe\UserGuide\Model\UserGuide;
 
 class UserGuideAdmin extends ModelAdmin
@@ -16,4 +18,12 @@ class UserGuideAdmin extends ModelAdmin
     private static $menu_icon_class = 'font-icon-block-content';
 
     private static $menu_title = 'User Guides';
+
+    protected function getGridFieldConfig(): GridFieldConfig
+    {
+        $config =  parent::getGridFieldConfig();
+        $config->removeComponentsByType(GridFieldAddNewButton::class);
+
+        return $config;
+    }
 }
