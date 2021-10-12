255afbefaf8875215a9ce109a4d382339a2f47a2
Phillip King <phillip.king@silverstripe.com>
2021-10-12 14:58:00 +1300

    Model User Guide

--- src/Model/UserGuide.php
+++ src/Model/UserGuide.php
@@ -3,8 +3,18 @@
 namespace SilverStripe\UserGuide\Model;
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\View\ViewableData;
 
 class UserGuide extends DataObject
 {
+    private static $db = [
+        'Title' => 'Varchar',
+        'PreNotes' => 'HTMLText',
+        'Content' => 'HTMLText',
+        'PostNotes' => 'HTMLText',
+    ];
 
+    private static $many_many = [
+        'Guides' => ViewableData::class
+    ];
 }
