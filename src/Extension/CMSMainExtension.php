f40a90fd645cc5295ced6c2517458440a36427b8
Marco Hermo <marco@silverstripe.com>
2021-12-06 15:49:46 +1300

    wip ;)

---
+++ src/Extension/CMSMainExtension.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SilverStripe\UserGuide\Extension;
+
+use SilverStripe\CMS\Controllers\CMSPageSettingsController;
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\ClassInfo;
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\UserGuide\Model\UserGuide;
+use SilverStripe\UserGuide\Controller\CMSUserGuideController;
+use Page;
+
+class CMSMainExtension extends Extension
+{
+
+    public function LinkPageUserGuide()
+    {
+        $owner = $this->getOwner();
+        if ($id = $owner->currentPageID()) {
+            return $owner->LinkWithSearch(
+                Controller::join_links(CMSUserGuideController::singleton()->Link('show'), $id)
+            );
+        } else {
+            return null;
+        }
+    }
+
+    public function IsUserGuideController()
+    {
+        return get_class($this->getOwner()) === CMSUserGuideController::class;
+    }
+
+    public function ShowUserGuide()
+    {
+        return HTMLEditorField::create('UserGuideContent', 'User Guide Content', $this->getUserGuideContent())
+            ->performReadonlyTransformation();
+    }
+
+    public function getUserGuideContent()
+    {
+        $pageID = $this->getOwner()->currentPageID();
+
+        if (!$pageID) {
+            return null;
+        }
+
+        $page = Page::get()->find('ID', $pageID);
+
+        if (!$page) {
+            return null;
+        }
+
+        $userguide = UserGuide::get()->find('DerivedClass', $page->ClassName);
+
+        if (!$userguide) {
+            return null;
+        }
+
+        return $userguide->Content;
+    }
+
+}
